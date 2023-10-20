efae8ca4c3d1b87bd1ec700469f2c77da264f8c0
FranZavalla <franzavalla85@gmail.com>
2023-10-20 12:35:20 -0300

    feat: added inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
 import { type AppType } from "next/app";
-
-import { api } from "~/utils/api";
+import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--inter-font",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --inter-font: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
