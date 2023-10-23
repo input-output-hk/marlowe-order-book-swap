f686dc0af16746b7dbed1da11a4ce771791ab330
Francisco Zavalla <franzavalla85@gmail.com>
2023-10-23 15:37:49 -0300

    Component/input (#3)
    * feat: implemented search bar functionality
    
    * feat: addapted input to numbers and date
    
    * feat: minor improvments to code legibility
    
    * feat: added padding right when 'endContent'
    
    * disabled eslint-rules for some files
    
    * feat: removed darker-gray
    
    * feat: added required option
    
    * fix: required deafault value

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,59 @@
+import { type InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  endContent?: JSX.Element;
+  label?: string;
+  pointerEvents?: boolean;
+  startContent?: JSX.Element;
+  useShadow?: boolean;
+}
+
+export const Input = ({
+  disabled = false,
+  endContent,
+  label,
+  max,
+  min,
+  placeholder,
+  pointerEvents: needsPointerEvents = false,
+  required,
+  startContent,
+  type = "text",
+  useShadow = false,
+  value,
+  onChange,
+}: InputProps) => {
+  const shadow = useShadow ? "shadow-md" : "";
+  const paddingLeftX = startContent ? "pl-12" : "pl-4";
+  const paddingRightX = endContent ? "pr-12" : "pr-4";
+  const pointerEvents = needsPointerEvents ? "" : "pointer-events-none";
+
+  return (
+    <>
+      {label && (
+        <label htmlFor={label} className="text-m-dark-gray">
+          {label}
+        </label>
+      )}
+      <div className="relative flex items-center">
+        <div className={`${pointerEvents} absolute p-4`}>{startContent}</div>
+        <input
+          type={type}
+          id={label}
+          placeholder={placeholder}
+          max={max}
+          min={min}
+          value={value?.toString()}
+          onChange={onChange}
+          onError={() => console.log("error")}
+          disabled={disabled}
+          required={required}
+          className={`m-dark-grey focus:border-m-purple focus:ring-m-purple w-full flex-grow rounded-md border p-3 ${paddingLeftX} ${paddingRightX} ${shadow} focus:outline-none focus:ring-1`}
+        />
+        <div className={`${pointerEvents} absolute right-0 p-4`}>
+          {endContent}
+        </div>
+      </div>
+    </>
+  );
+};
