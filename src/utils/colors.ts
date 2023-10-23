39df6543a0110f1bae699a075558787d1dd6e526
Sara <88407794+mikarasv@users.noreply.github.com>
2023-10-23 14:34:34 -0300

    feat: buttons (#2)
    * feat: buttons
    
    * colors and button attributes
    
    * feat: adding unnecesary colors
    
    * feat: color corrections and dynamic class using
    
    * fix: button bug on filled and hovered

---
+++ src/utils/colors.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  BLACK: "#000000",
+  DISABLED: "#979DAF",
+  DARK_GRAY: "#8C8C8C",
+  GREEN: "#17A98F",
+  PURPLE: "#4B1FED",
+  RED: "#FF525B",
+  WHITE: "#FFFFFF",
+} as const;
+
+export type COLORS = (typeof COLORS)[keyof typeof COLORS];
+
+export const stringedColors: Record<COLORS, string> = {
+  [COLORS.BLACK]: "black",
+  [COLORS.DISABLED]: "m-disabled",
+  [COLORS.DARK_GRAY]: "m-dark-gray",
+  [COLORS.GREEN]: "m-green",
+  [COLORS.PURPLE]: "m-purple",
+  [COLORS.RED]: "m-red",
+  [COLORS.WHITE]: "white",
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,21 @@ export default {
       fontFamily: {
         inter: "var(--font-inter)",
       },
+      colors: {
+        "m-disabled": "#979DAF",
+        "m-dark-gray": "#8C8C8C",
+        "m-green": "#17A98F",
+        "m-purple": "#4B1FED",
+        "m-red": "#FF525B",
+      },
     },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(text|bg|border)-(m-disabled|m-dark-gray|m-green|m-purple|m-red)/,
+      variants: ["hover"],
+    },
+  ],
 } satisfies Config;
