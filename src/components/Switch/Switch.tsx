7141e675f97f7f9c985bc46c3bef5ad01b08e35a
FranZavalla <franzavalla85@gmail.com>
2023-10-27 15:32:29 -0300

    feat: added switch component

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,33 @@
+import type { Dispatch, SetStateAction } from "react";
+
+interface SwitchProps {
+  enabled: boolean;
+  setEnabled: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Switch = ({ enabled, setEnabled }: SwitchProps) => {
+  return (
+    <div className="relative flex h-full flex-col items-center justify-center overflow-hidden">
+      <div className="flex">
+        <label className="relative inline-flex cursor-pointer items-center gap-2">
+          <input
+            type="checkbox"
+            className="peer sr-only"
+            checked={enabled}
+            readOnly
+          />
+          <div
+            onClick={() => {
+              setEnabled(!enabled);
+            }}
+            className="
+              after:border-m-light-gray peer h-7 w-12 rounded-full
+              bg-gray-300 after:absolute after:left-1 after:top-1 after:h-5
+              after:w-5 after:rounded-full after:border after:bg-white after:transition-all after:content-['']
+              peer-checked:bg-m-green peer-checked:after:translate-x-full"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
