b9be9c820539c87e013326b77316baf2b94cd42a
sara <victoriasarakim.sk@gmail.com>
2023-11-01 14:00:26 -0300

    feat: responsive and reallocation of files

--- src/components/Information/CompleteSwap.tsx
+++ src/components/Information/CompleteSwap.tsx
@@ -9,7 +9,7 @@ import { Button, SIZE } from "../Button/Button";
 export const CompleteSwap = () => {
   return (
     <main className="flex h-full w-full flex-col items-center text-center">
-      <div className="m-auto mt-3 flex w-2/3 flex-col items-center justify-center gap-5 align-middle sm:w-1/3">
+      <div className="m-auto mt-3 flex w-2/3 flex-col items-center justify-center gap-5 align-middle lg:w-1/3">
         <Image src={CompleteIcon as string} height={60} alt="" />
         <div className="text-3xl font-extrabold">
           Nice, you’ve completed a swap! 🎉
@@ -20,15 +20,21 @@ export const CompleteSwap = () => {
         </div>
         <div className="flex w-4/5 flex-col gap-7">
           <Button size={SIZE.REGULAR} filled>
-            <div className="flex justify-center gap-3">
-              <Image src={CubeIcon as string} height={20} alt="" />
+            <div className="flex justify-center gap-3 whitespace-normal break-words text-sm sm:text-base">
+              <div className="hidden sm:block">
+                <Image src={CubeIcon as string} height={20} alt="" />
+              </div>
               View transaction
-              <Image src={SeeMoreIcon as string} height={20} alt="→" />
+              <div className="hidden sm:block">
+                <Image src={SeeMoreIcon as string} height={20} alt="→" />
+              </div>
             </div>
           </Button>
           <Button size={SIZE.REGULAR} color={COLORS.BLACK}>
-            <div className="flex justify-center gap-3">
-              <Image src={LeftArrowIcon as string} height={20} alt="" />
+            <div className="flex justify-center gap-3 whitespace-normal break-words text-sm sm:text-base">
+              <div className="hidden sm:block">
+                <Image src={LeftArrowIcon as string} height={20} alt="" />
+              </div>
               Return to vesting schedule
             </div>
           </Button>
