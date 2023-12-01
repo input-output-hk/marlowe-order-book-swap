df9964d0446aff9da4676881ffdb6e4a3fe197e8
Francisco Zavalla <franzavalla85@gmail.com>
2023-12-01 16:37:36 -0300

    Bugfix/error responsive & balance (#77)
    * fix: added responsive
    
    * fix: get decimals from balance

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -9,9 +9,9 @@ interface ErrorMessageProps {
 export const ErrorMessage = ({ message }: ErrorMessageProps) => {
   return (
     <main className="flex h-fit w-full flex-grow flex-col items-center justify-center gap-4">
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 px-28 py-48">
         <Image src={ErrorIcon as string} alt="Error" height={ICON_SIZES.XXL} />
-        <p className="text-2xl text-m-disabled">{message}</p>
+        <p className="text-center text-2xl text-m-disabled">{message}</p>
       </div>
     </main>
   );
