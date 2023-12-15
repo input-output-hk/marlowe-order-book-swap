21537b86145c62f91542cb00dc70205b442219a7
FranZavalla <franzavalla85@gmail.com>
2023-12-15 15:04:20 -0300

    feat: updating .env and readme files

--- src/env.mjs
+++ src/env.mjs
@@ -26,9 +26,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_OWN_ADDRESS: z.string().default("addr_test123456789"),
-    NEXT_PUBLIC_BLOCKFROST_PROJECT_ID: z.string(),
-    NEXT_PUBLIC_DAPP_ID: z.string().default("marlowe.examples.swap.prototype"),
+    NEXT_PUBLIC_DAPP_ID: z.string(),
     NEXT_PUBLIC_RUNTIME_URL: z.string(),
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
@@ -40,9 +38,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_OWN_ADDRESS: process.env.NEXT_PUBLIC_OWN_ADDRESS,
-    NEXT_PUBLIC_BLOCKFROST_PROJECT_ID:
-      process.env.NEXT_PUBLIC_BLOCKFROST_PROJECT_ID,
     NEXT_PUBLIC_DAPP_ID: process.env.NEXT_PUBLIC_DAPP_ID,
     NEXT_PUBLIC_RUNTIME_URL: process.env.NEXT_PUBLIC_RUNTIME_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
